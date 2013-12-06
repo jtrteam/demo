caa8fa1f3ed6de55b19787bbfa16628bfcdacbb2
Santhosh <santhosh@gamillc.com>
2013-12-06 20:08:27 +0530

    sizemetrix update

---
+++ app/code/local/Whlly/Common/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Whlly
+ * @category   Whlly
+ * @package    Whlly_Music
+ * @copyright  Copyright (c) 2013-2014 Whlly. (http://www.whlly.com)
+ * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+class Whlly_Common_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    
+}
---
+++ app/code/local/Whlly/Sizematrix/Block/Sizematrix.php
@@ -0,0 +1,10 @@
+<?php
+class Whlly_Sizematrix_Block_Sizematrix extends Mage_Core_Block_Template
+{
+	public function _prepareLayout()
+    {
+		return parent::_prepareLayout();
+    }
+    
+    
+}
